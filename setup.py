fb71fca88b1a780e9c5d9c3caeba3f74b3bd97f3
Uzmaa7 <uk9971khan@gmail.com>
2025-10-28 16:43:27 +0530

    Project Structure Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RxAI',
+    version= '0.0.0',
+    author= 'Uzma Khan',
+    author_email= 'uk9971khan@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
